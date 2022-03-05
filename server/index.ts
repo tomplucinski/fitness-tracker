0a6487eb80d0d371f577b043b76093aad82898cb
Tom Plucinski <tplucinski@coolerscreens.com>
2022-03-05 15:33:03 -0700

    Create startup scripts

--- server/index.ts
+++ server/index.ts
@@ -4,8 +4,8 @@ const app = express()
 
 const PORT = process.env.PORT || 4500
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/api', (req, res) => {
+  res.send('Hello World!!')
 })
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`))
