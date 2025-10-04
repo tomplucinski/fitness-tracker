2dad8328a7537449bd6a1f79ad45261e74750274
Tom Plucinski <tapluci1@gmail.com>
2025-10-04 17:23:44 -0400

    Add hello world endpoint

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def hello_world(request):
+    return JsonResponse({"message": "Hello, world!"})
