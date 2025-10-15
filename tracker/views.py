63e4713e6e288cc8afe49a3fc74b1e55447a20fa
Tom Plucinski <tapluci1@gmail.com>
2025-10-14 20:29:41 -0400

    Add get exercises view

--- tracker/views.py
+++ tracker/views.py
@@ -1,7 +1,8 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import WorkoutSerializer
+from .serializers import WorkoutSerializer, ExerciseSerializer
+from .models import Exercise
 
 #need error handling
 @api_view(['POST'])
@@ -12,3 +13,9 @@ def create_workout(request):
     workout = serializer.save()
     return Response(WorkoutSerializer(workout).data, status=status.HTTP_201_CREATED)
     
+@api_view(['GET'])
+def get_exercises(request):
+    """Get all exercises"""
+    exercises = Exercise.objects.all()
+    serializer = ExerciseSerializer(exercises, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
